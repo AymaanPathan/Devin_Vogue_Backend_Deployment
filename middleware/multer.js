5be5c2fa7da14407b941d3e06dd06cd453a21a08
Aymaan <aymaanpathan5@gmail.com>
2024-11-03 23:15:02 +0530

    add memoryStorage

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,9 +1,6 @@
 const multer = require("multer");
 
-const storage = multer.diskStorage({
-  filename: function (res, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+const storage = multer.memoryStorage(); // Change to in-memory storage
 const upload = multer({ storage: storage });
+
 module.exports = upload;
